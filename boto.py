409d7464b9f766961037e68f7fef4d3893bdca06
rcodin2 <89382289+rcodin2@users.noreply.github.com>
2022-06-03 13:14:55 +0200

    Delete boto.py

--- boto.py
+++
@@ -1,9 +0,0 @@
-import machine
-import time
-
-button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
-
-while True:
-  if button.value() == 1:
-    print("You pressed the button!")
-    time.sleep(2)
